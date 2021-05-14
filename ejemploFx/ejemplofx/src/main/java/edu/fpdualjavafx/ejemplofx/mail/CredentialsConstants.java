0b63df85a349727dbbdad0e7d65765a03d7aa839
twlster <jose.m.prieto.villar@accenture.com>
2021-05-14 12:15:22 +0200

    Sending Emails :).

---
+++ ejemploFx/ejemplofx/src/main/java/edu/fpdualjavafx/ejemplofx/mail/CredentialsConstants.java
@@ -0,0 +1,14 @@
+package edu.fpdualjavafx.ejemplofx.mail;
+
+
+/**
+ * credentials Constants class.
+ * @author jose.m.prieto.villar
+ *
+ */
+public class CredentialsConstants {
+
+	public static final String PASSWD = "mail.smtp.password";
+	public static final String USER = "mail.smtp.user";
+	
+}
